b2f01078d96e9738d99f12726ba59b9c8e61c57f
이주현 <juhyun@ijuhyeon-ui-MacBookAir.local>
2022-01-25 23:05:26 +0900

    fix: successfully get item without jumping

--- Assets/Scripts/itemBox.cs
+++ Assets/Scripts/itemBox.cs
@@ -10,6 +10,7 @@ public class itemBox : MonoBehaviour
     public PlayerController rudeZbangController;
     float collisionTime;
     float speed;
+    float strafeSpeed;
     int number;
     bool haveItem;
     GameObject canvas;
@@ -18,84 +19,84 @@ public class itemBox : MonoBehaviour
     {
         canvas = GameObject.Find("items");
         speed = rudeZbangController.speed;
+        strafeSpeed = rudeZbangController.strafeSpeed;
         left = rudeZbangController.left;
         right = rudeZbangController.right;
         front = rudeZbangController.front;
         back = rudeZbangController.back;
         haveItem = false;
-        // StartCoroutine(Item());
+        StartCoroutine(Item());
 
     }
     void FixedUpdate()
     {
-        if (haveItem)
-        {
-            switch (number)
-            {
-                case 0:
-                    StartCoroutine(Boost());
-                    Debug.Log("finish coroutine : " + haveItem);
-                    break;
-                case 1:
-                    StartCoroutine(Slow());
-                    Debug.Log("finish coroutine : " + haveItem);
-                    break;
-                case 2:
-                    StartCoroutine(Stop());
-                    Debug.Log("finish coroutine : " + haveItem);
-                    break;
-                case 3:
-                    StartCoroutine(Change());
-                    Debug.Log("finish coroutine : " + haveItem);
-                    break;
-                case 4:
-                    StartCoroutine(Jump());
-                    Debug.Log("finish coroutine : " + haveItem);
-                    break;
-            }
-        }
+        // if (haveItem)
+        // {
+        //     switch (number)
+        //     {
+        //         case 0:
+        //             StartCoroutine(Boost());
+        //             Debug.Log("finish coroutine : " + haveItem);
+        //             break;
+        //         case 1:
+        //             StartCoroutine(Slow());
+        //             Debug.Log("finish coroutine : " + haveItem);
+        //             break;
+        //         case 2:
+        //             StartCoroutine(Stop());
+        //             Debug.Log("finish coroutine : " + haveItem);
+        //             break;
+        //         case 3:
+        //             StartCoroutine(Change());
+        //             Debug.Log("finish coroutine : " + haveItem);
+        //             break;
+        //         case 4:
+        //             StartCoroutine(Jump());
+        //             Debug.Log("finish coroutine : " + haveItem);
+        //             break;
+        //     }
+        // }
     }
-    // IEnumerator Item()
-    // {
-    //     while (true)
-    //     {
-    //         yield return null;
-    //         if (haveItem)
-    //         {
-    //             haveItem = false;
-    //             Debug.Log("haveItem coroutine haveItem : " + number);
-    //             switch (number)
-    //             {
-    //                 case 0:
-    //                     StartCoroutine(Boost());
-    //                     Debug.Log("Finish Boost coroutine");
-    //                     break;
-    //                 case 1:
-    //                     StartCoroutine(Slow());
-    //                     Debug.Log("Finish Slow coroutine");
-    //                     break;
-    //                 case 2:
-    //                     StartCoroutine(Stop());
-    //                     Debug.Log("Finish Stop coroutine");
-    //                     break;
-    //                 case 3:
-    //                     StartCoroutine(Change());
-    //                     Debug.Log("Finish Change coroutine");
-    //                     break;
-    //                 case 4:
-    //                     StartCoroutine(Jump());
-    //                     Debug.Log("Finish Jump coroutine");
-    //                     break;
-    //             }
-    //         }
+    IEnumerator Item()
+    {
+        while (true)
+        {
+            yield return null;
+            if (haveItem)
+            {
+                // Debug.Log("haveItem coroutine haveItem : " + number);
+                switch (number)
+                {
+                    case 0:
+                        yield return StartCoroutine(Boost());
+                        Debug.Log("Finish Boost coroutine");
+                        break;
+                    case 1:
+                        yield return StartCoroutine(Slow());
+                        Debug.Log("Finish Slow coroutine");
+                        break;
+                    case 2:
+                        yield return StartCoroutine(Stop());
+                        Debug.Log("Finish Stop coroutine");
+                        break;
+                    case 3:
+                        yield return StartCoroutine(Change());
+                        Debug.Log("Finish Change coroutine");
+                        break;
+                    case 4:
+                        yield return StartCoroutine(Jump());
+                        Debug.Log("Finish Jump coroutine");
+                        break;
+                }
+            }
 
-    //     }
+        }
 
-    // }
+    }
     IEnumerator Boost()
     {
-        // Debug.Log("Start Boost");
-        rudeZbangController.speed = speed*3;
+        Debug.Log("immediately after boost : " + haveItem);
+        rudeZbangController.speed = speed*2;
         for (int i=5; i>0; i--)
         {
             canvas.transform.Find("BoostPanel").Find("time").GetComponent<Text>().text = i + "s";
@@ -104,12 +105,13 @@ public class itemBox : MonoBehaviour
         canvas.transform.Find("BoostPanel").gameObject.SetActive(false);
         rudeZbangController.speed = speed;
         haveItem = false;
+        Debug.Log("after boost : " + haveItem);
     }
 
     IEnumerator Slow()
     {
-        // Debug.Log("Start Slow");
-        rudeZbangController.speed = speed/2;
+        Debug.Log("immediately after slow : " + haveItem);
+        rudeZbangController.speed = speed/3;
         for (int i=5; i>0; i--)
         {
             canvas.transform.Find("SlowPanel").Find("time").GetComponent<Text>().text = i + "s";
@@ -119,12 +121,14 @@ public class itemBox : MonoBehaviour
 
         rudeZbangController.speed = speed;
         haveItem = false;
+        Debug.Log("after slow : " + haveItem);
     }
 
     IEnumerator Stop()
     {
-        // Debug.Log("Start  Stop");
+        Debug.Log("immediately after stop : " + haveItem);
         rudeZbangController.speed = 0;
+        rudeZbangController.strafeSpeed = 0;
         for (int i=5; i>0; i--)
         {
             canvas.transform.Find("StopPanel").Find("time").GetComponent<Text>().text = i + "s";
@@ -133,12 +137,14 @@ public class itemBox : MonoBehaviour
         canvas.transform.Find("StopPanel").gameObject.SetActive(false);
     
         rudeZbangController.speed = speed;
+        rudeZbangController.strafeSpeed = strafeSpeed;
         haveItem = false;   
+        Debug.Log("after stop : " + haveItem);
     }
 
     IEnumerator Change()
     {
-        // Debug.Log("Start Change");
+        Debug.Log("immediately after change : " + haveItem);
         rudeZbangController.left = right;
         rudeZbangController.right = left;
         rudeZbangController.front = back;
@@ -149,27 +155,27 @@ public class itemBox : MonoBehaviour
             yield return new WaitForSecondsRealtime(1f);
         }
         canvas.transform.Find("ConversionPanel").gameObject.SetActive(false);
-        yield return new WaitForSeconds(3f);
         rudeZbangController.left = left;
         rudeZbangController.right = right;
         rudeZbangController.front = front;
         rudeZbangController.back = back;
         haveItem = false;
+        Debug.Log("after change : " + haveItem);
     }
 
     IEnumerator Jump()
     {
-        // Debug.Log("Start Jump");
+        Debug.Log("immediately after jump : " + haveItem);
+        Rigidbody rb = transform.root.GetComponentInChildren<Rigidbody>();        
+        rb.AddForce(new Vector3(0,100,2000));   
         yield return new WaitForSecondsRealtime(3f);
-        canvas.transform.Find("JumpPanel").gameObject.SetActive(false);
-        Rigidbody rb = transform.root.GetComponentInChildren<Rigidbody>();
-        // Debug.Log(rb);
-        rb.AddForce(new Vector3(0,300,100));        
+        canvas.transform.Find("JumpPanel").gameObject.SetActive(false);     
         haveItem = false;
+        Debug.Log("after jump : " + haveItem);
     }
 
     void OnTriggerEnter(Collider collision){
-        // Debug.Log("Before check null : " + haveItem);
+        Debug.Log("before check haveitem after collision : " + haveItem);
         // Debug.Log("test");
         if (!haveItem)
         {
